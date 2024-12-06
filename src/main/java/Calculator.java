c78d2f57720b735a39eeb64d10ab24789ad70daa
Zakharenko Nikita <121438934+superboyAmira@users.noreply.github.com>
2024-12-06 14:21:47 +0300

    fix

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,29 +1,5 @@
 public class Calculator {
-
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int dif(int a, int b) {
-        return a - b;
-    }
-
-    public int div(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Division by zero is not allowed");
-        }
-        return a / b;
-    }
-
-    public int times(int a, int b) {
-        return a * b;
-    }
-
-    public int solver() {
-        int result = add(2, times(3, 4)); 
-        result = dif(result, div(6, 2));
-        return result;
-
+	
     public int solver(int a, int b, int c) {
         int numberTimes = times(a, b);
         int numberDiv = div(c, a);
@@ -42,11 +18,12 @@ public class Calculator {
         if (b == 0) {
             throw new IllegalArgumentException("Divide by zero error");
         }
+
         return a / b;
     }
 
     public int times(int a, int b) {
         return a * b;
-
     }
+
 }
