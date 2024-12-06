5c97b0b072c9d3de25b91013133254fb6fecb476
krissstiina <162792967+krissstiina@users.noreply.github.com>
2024-12-06 13:34:15 +0300

    change Readme,Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,26 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver() {
+        int result = add(2, times(3, 4)); // 2 + (3 * 4)
+        result = dif(result, div(6, 2));
+        return result;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,30 +2,44 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
+
+    Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(0, calculator.add(-2, 2));
+        assertEquals(100, calculator.add(50,50));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(-1, calculator.dif(2, 3));
+        assertEquals(4, calculator.dif(2, -2));
+        assertEquals(0, calculator.dif(50,50));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(2, calculator.div(4, 2));
+        assertThrows(ArithmeticException.class, () -> calculator.div(4, 0));
+        assertEquals(1, calculator.div(5,5));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(6, calculator.times(2, 3));
+        assertEquals(0, calculator.times(5,0));
+        assertEquals(25, calculator.times(5,5));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Calculator calculator = new Calculator();
+        assertEquals(11, calculator.solver());
     }
 }
