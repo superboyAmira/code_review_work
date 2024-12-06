4d77de1f08133f22baaebc6a680afd44a4cbd9fe
KsushaGruzdeva <135704686+KsushaGruzdeva@users.noreply.github.com>
2024-12-06 13:18:31 +0300

    change realization in files README.md, Calculator.java, CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,30 +2,44 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
+
+    Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(7, calculator.add(4, 3));
+        assertEquals(10, calculator.add(-20, 10));
+        assertEquals(35, calculator.add(25,10));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(-3, calculator.dif(2, 5));
+        assertEquals(-4, calculator.dif(1, 5));
+        assertEquals(77, calculator.dif(78,1));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(3, calculator.div(6, 2));
+        assertEquals(77, calculator.div(77,1));
+        assertThrows(ArithmeticException.class, () -> calculator.div(6, 0));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(15, calculator.times(3, 5));
+        assertEquals(18, calculator.times(6,3));
+        assertEquals(0, calculator.times(89,0));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Calculator calculator = new Calculator();
+        assertEquals(16, calculator.solver());
     }
 }
